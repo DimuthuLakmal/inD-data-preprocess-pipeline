d0f2c3344840ec1948bbb610c03f3faee0c7bb0f
DimuthuLakmal <kjtdimuthu.13@cse.mrt.ac.lk>
2025-09-09 14:21:09 +1000

    local changes

--- src/dataset/dataset.py
+++ src/dataset/dataset.py
@@ -47,9 +47,9 @@ class OGMDataset(Dataset):
         self.frame_to_track_idxs = {}
         self.class_dict = {'car': 0, 'truck_bus': 1, 'bicycle': 2, 'pedestrian': 3}
 
-        start_scene = 2
-        end_scene = 6
-        filename = "index_map2.pkl"
+        start_scene = 18
+        end_scene = 19
+        filename = "index_map5.pkl"
 
         self.data_dict = {}
 
--- src/view_data.py
+++ src/view_data.py
@@ -332,9 +332,9 @@ background_images = {}
 fixed_blocks_info = {}
 frame_to_track_idxs = {}
 
-start_scene = 7
-end_scene = 8
-filename = "index_map3.pkl"
+start_scene = 18
+end_scene = 19
+filename = "index_map5.pkl"
 
 data_dict_all = {}
 
@@ -364,11 +364,11 @@ for idx in range(len(keys)):
     current_frame = int(key_elements[1])
     ego_vehicle_track_idx = int(key_elements[2])
 
-    skip_until_scene_id = 8
+    skip_until_scene_id = 19
     if skip_until_scene_id != -1 and scene_id < skip_until_scene_id:
         continue
 
-    skip_until_frame = 25452
+    skip_until_frame = 2022
     if skip_until_frame != -1 and current_frame < skip_until_frame:
         continue
 
